71597a61bc5c7433740e09e3ccbc2b918cf12645
kwurty <k.soroka@gmail.com>
2021-10-15 21:08:36 -0500

    restructuring code to make more sense

--- src/components/Host.js
+++ src/components/Host.js
@@ -1,17 +1,14 @@
 // eslint-disable-next-line
-import React, { useContext, useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Players from './Players';
+import { Redirect } from "react-router-dom";
 import { SocketContext } from '../utilities/connect';
 
-export default function Host() {
-    const socket = useContext(SocketContext);
-    const [roomCode, setroomCode] = useState('');
+export default function Host({ player, updateplayer, updategamesettings, socket }) {
     const [gameSettings, setGameSettings] = useState(false);
-    const [displaySettings, setDisplaySettings] = useState(false);
     const [username, setUsername] = useState('');
-    const [submittedName, setSubmittedName] = useState(false);
+    const maxlength = 8;
 
-    const lastUsername = useRef(' ');
 
     function updateGameSettings(key, value) {
         const newObject = {
@@ -31,130 +28,43 @@ export default function Host() {
 
 
     useEffect(() => {
-
-        socket.on('connect', () => {
-        })
         socket.on('returnRoomCode', (roomcode, gamesettings) => {
-            // When the room code is returned, we are going to set the room code then request the game settings
-            setroomCode(roomcode);
-            setGameSettings(JSON.parse(gamesettings));
-            setDisplaySettings(JSON.parse(gamesettings));
+            updateplayer({
+                ...player,
+                name: username,
+                room: roomcode
+            });
+            updategamesettings(gamesettings)
         })
-        socket.on('returnGameSettings', (settings) => {
-            setDisplaySettings(JSON.parse(settings));
-        })
-        socket.on('userJoinRoom', (players) => {
-
-        })
-
     }, []);
 
-    useEffect(() => {
-        socket.emit('updateGameSettings', roomCode, JSON.stringify(gameSettings));
-    }, [gameSettings]);
+    // useEffect(() => {
+    //     socket.emit('updateGameSettings', roomCode, JSON.stringify(gameSettings));
+    // }, [gameSettings]);
 
-    useEffect(() => {
-        setDisplaySettings(gameSettings)
-    }, [gameSettings]);
+    // useEffect(() => {
+    //     setDisplaySettings(gameSettings)
+    // }, [gameSettings]);
 
     return (
         <div className="w-full min-h-screen flex justify-center items-center flex-col bg-gray-500">
-
-            {
-                submittedName && gameSettings ?
-                    <div>
-                        <div>
-                            <h1 className="text-white text-4xl"> {roomCode} </h1>
-                        </div>
-                        <form>
-                            Max Players:
-                            <select value={gameSettings.MAX_PLAYERS} onChange={(e) => {
-                                updateGameSettings('MAX_PLAYERS', parseInt(e.target.value));
-                            }}>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                            </select>
-
-                            Game Mode:
-                            <select value={gameSettings.GAME_MODE} onChange={(e) => {
-                                updateGameSettings('GAME_MODE', e.target.value);
-                            }}>
-
-                                <option value="lives"> Limited Lives </option>
-                                <option value="unlimited"> Unlimited Play </option>
-                            </select>
-
-                            {
-                                gameSettings.GAME_MODE === "lives" ?
-                                    (
-                                        <div>
-                                            Number of lives:
-                                            <input type="number" min="1" max="8" value={gameSettings.MAX_LIVES} onChange={(e) => {
-                                                updateGameSettings('MAX_LIVES', parseInt(e.target.value));
-                                            }}>
-                                            </input>
-                                        </div>
-                                    )
-                                    :
-                                    null
-                            }
-
-                            <div>
-                                Time to Answer:
-                                <input type="number" min="5" max="60" value={gameSettings.ANSWER_TIMER} onChange={(e) => {
-                                    updateGameSettings('ANSWER_TIMER', parseInt(e.target.value));
-                                }}>
-                                </input> seconds
-                            </div>
-                            <div className="block">
-                                Max players - {displaySettings.MAX_PLAYERS}
-                            </div>
-                            <div className="block">
-                                Time to Answer - {displaySettings.ANSWER_TIMER}
-                            </div>
-                            <div className="block">
-                                Game Mode? - {displaySettings.GAME_MODE}
-                            </div>
-
-                            <button className="border rounded-lg bg-blue-400 mx-5 px-10 py-5" onClick={() => {
-                                socket.emit("startGame", roomCode);
-                            }}> Start </button>
-
-                        </form>
-
-                    </div>
-                    :
-                    <div>
-                        <div>
-                            <h1 className="pb-5 text-4xl text-white">Name:</h1>
-                            <div className="flex align-middle justify-center">
-                                <input type="text" id="name" className="rounded-md py-5 w-11/12 uppercase" value={username} maxLength="8" onChange={(e) => {
-                                    const { value, maxLength } = e.target;
-                                    setUsername(value.slice(0, maxLength).toUpperCase());
-                                }} ref={lastUsername} />
-                                <label htmlFor="name" className="text-white self-center pl-1"> ({8 - username.length})</label>
-                                <button className="border rounded-lg bg-blue-400 mx-5 px-10 py-5" onClick={() => {
-                                    socket.emit("generateRoom", username);
-                                    setSubmittedName(true);
-                                }}> Host </button>
-                            </div>
-
-                        </div>
-                    </div>
-            }
-
-
-            {
-                gameSettings.PLAYERS && gameSettings.PLAYERS.length > 0 &&
+            <div>
                 <div>
-                    <Players players={gameSettings.PLAYERS} />
+                    <h1 className="pb-5 text-4xl text-white">Name:</h1>
+                    <div className="flex align-middle justify-center">
+                        <input type="text" id="name" className="rounded-md py-5 w-11/12 uppercase" value={username} maxLength={maxlength} onChange={(e) => {
+                            const { value } = e.target;
+                            setUsername(value.slice(0, maxlength).toUpperCase());
+                        }} />
+                        <label htmlFor="name" className="text-white self-center pl-1"> ({maxlength - username.length})</label>
+                        <button className="border rounded-lg bg-blue-400 mx-5 px-10 py-5" onClick={(e) => {
+                            e.preventDefault();
+                            socket.emit("generateRoom", username);
+                        }}> Host </button>
+                    </div>
+
                 </div>
-            }
+            </div>
         </div>
     )
 }
