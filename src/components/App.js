381af87c99791419d24a4bc0803b03f69bfed6ee
Kurtis <k.soroka@gmail.com>
2021-10-19 19:43:12 -0500

    updates for host/pregame

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import Login from './Login';
 import Host from './Host';
 import Game from './Game';
+import Temp from './Temp';
 import Pregame from './Pregame'
 import { socket, SocketContext } from '../utilities/connect';
 
@@ -9,13 +10,12 @@ import { socket, SocketContext } from '../utilities/connect';
 function App() {
 
   // declare game states
-  let [PLAYER, SET_PLAYER] = useState({
+  const [PLAYER, SET_PLAYER] = useState({
     id: undefined,
     name: undefined,
     connected: false,
     host: undefined,
     state: 0,
-    roomcode: undefined
   })
   let [GAMESETTINGS, SET_GAMESETTINGS] = useState(undefined)
   let [PLAYERS, SET_PLAYERS] = useState()
@@ -24,25 +24,32 @@ function App() {
   })
   const client = socket
 
+  // const SET_GAMESETTINGS_HELPER = (new)
+
   // watch for game updates
   useEffect(() => {
-    client.on('connect', () => {
-      SET_PLAYER({ ...PLAYER, id: socket.id, connected: true })
+    client.once('connect', () => {
+      SET_PLAYER((player) => {
+        return { ...player, id: socket.id, connected: true }
+      });
     });
     client.on('disconnect', () => {
       SET_PLAYER({ ...PLAYER, connected: false })
     });
-    client.on('returnRoomCode', (roomcode) => {
-      // SET_PLAYER({ ...PLAYER, ishost: true, roomcode: roomcode })
+    client.once('returnRoomCode', (roomcode, gamesettings) => {
+      SET_PLAYER((player) => {
+        return { ...player, room: roomcode }
+      });
+      SET_GAMESETTINGS(gamesettings);
     });
     client.on('returnGameSettings', (gamesettings) => {
-      console.dir(JSON.parse(gamesettings));
-      // SET_GAMESETTINGS(JSON.parse(gamesettings))
+      SET_GAMESETTINGS(JSON.parse(gamesettings));
     });
     client.on('returnPlayerList', (players) => {
 
     })
-  }, [])
+  }, [client])
+
 
   // render the game based on settings
   return (
@@ -54,17 +61,19 @@ function App() {
         )}
 
         {/* render host */}
-        {PLAYER.connected && !GAMESTATE.active && PLAYER.host && !PLAYER.name && (
-          <Host socket={client} player={PLAYER} updateplayer={SET_PLAYER} gamesettings={GAMESETTINGS} updategamesettings={SET_GAMESETTINGS} />
+        {PLAYER.connected && !GAMESTATE.active && PLAYER.host && !PLAYER.name && !GAMESETTINGS && (
+          <Host socket={client} player={PLAYER} updateplayer={SET_PLAYER} />
         )}
 
-        {PLAYER.connected && !GAMESTATE.active && PLAYER.name && GAMESETTINGS && (
-          <Pregame player={PLAYER} players={PLAYERS} updategamesettings={SET_GAMESETTINGS} />
+        {PLAYER.connected && !GAMESTATE.active && PLAYER.name && (
+          <Pregame player={PLAYER} gamesettings={GAMESETTINGS} players={PLAYERS} updategamesettings={SET_GAMESETTINGS} />
         )}
 
-        {PLAYER.connected && GAMESTATE.active && (
+        {/* {PLAYER.connected && GAMESTATE.active && (
           <Game player={PLAYER} players={PLAYERS} gamesettings={GAMESETTINGS} gamestate={GAMESTATE} />
-        )}
+        )} */}
+
+        <Temp player={PLAYER} />
       </div>
     </SocketContext.Provider>
   )
--- src/components/Host.js
+++ src/components/Host.js
@@ -1,50 +1,9 @@
 // eslint-disable-next-line
 import React, { useState, useRef, useEffect } from 'react';
-import Players from './Players';
-import { Redirect } from "react-router-dom";
-import { SocketContext } from '../utilities/connect';
 
-export default function Host({ player, updateplayer, updategamesettings, socket }) {
-    const [gameSettings, setGameSettings] = useState(false);
+export default function Host({ socket, player, updateplayer }) {
     const [username, setUsername] = useState('');
     const maxlength = 8;
-
-
-    function updateGameSettings(key, value) {
-        const newObject = {
-            ...gameSettings,
-            [key]: value
-        }
-        setGameSettings(newObject)
-    }
-
-    function setHostName() {
-        updateGameSettings('PLAYERS', [{
-            id: socket.id,
-            name: username
-        }]);
-        setSubmittedName(true);
-    }
-
-
-    useEffect(() => {
-        socket.on('returnRoomCode', (roomcode, gamesettings) => {
-            updateplayer({
-                ...player,
-                name: username,
-                room: roomcode
-            });
-            updategamesettings(gamesettings)
-        })
-    }, []);
-
-    // useEffect(() => {
-    //     socket.emit('updateGameSettings', roomCode, JSON.stringify(gameSettings));
-    // }, [gameSettings]);
-
-    // useEffect(() => {
-    //     setDisplaySettings(gameSettings)
-    // }, [gameSettings]);
 
     return (
         <div className="w-full min-h-screen flex justify-center items-center flex-col bg-gray-500">
@@ -54,12 +13,17 @@ export default function Host({ player, updateplayer, updategamesettings, socket 
                     <div className="flex align-middle justify-center">
                         <input type="text" id="name" className="rounded-md py-5 w-11/12 uppercase" value={username} maxLength={maxlength} onChange={(e) => {
                             const { value } = e.target;
-                            setUsername(value.slice(0, maxlength).toUpperCase());
+                            let tempUserName = value.slice(0, maxlength).toUpperCase();
+                            setUsername(tempUserName);
+
                         }} />
                         <label htmlFor="name" className="text-white self-center pl-1"> ({maxlength - username.length})</label>
                         <button className="border rounded-lg bg-blue-400 mx-5 px-10 py-5" onClick={(e) => {
                             e.preventDefault();
-                            socket.emit("generateRoom", username);
+                            updateplayer((player) => {
+                                return { ...player, name: username }
+                            });
+                            socket.emit('generateRoom', username);
                         }}> Host </button>
                     </div>
 
