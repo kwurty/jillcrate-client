ea00b4f470c9c009a92d0384389387edb6638ca2
Kurtis <k.soroka@gmail.com>
2021-10-22 15:39:07 -0400

    pregame working

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { SocketContext } from '../utilities/connect'
 
-export default function Game(game) {
-    let GAME = game;
+export default function Game({ gamesettings, player }) {
     return (
         <div className="container">
             {
-                GAME.PLAYERS.map((player, index) => {
-                    <div className="player" key={player.id}>
-                        {player.name}
-                    </div>
-                })
-            }
 
+            }
         </div>
     )
 }
